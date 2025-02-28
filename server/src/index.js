bce9e46abc290d67c324109ec5759485da937997
R-Kri <rubrangsokri@gmail.com>
2025-02-28 12:15:54 +0530

    added basic sequelize and cli related files

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const {ServerConfig, Logger} = require('./config');
+const apiRoute = require('./routes');
+
+
+const app = express();
+
+app.use('/api', apiRoute);
+
+app.listen(ServerConfig.PORT, ()=>{
+    console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+});
+
